a3a79c9a595f5662ea1beb2e264a9fdf0593d5df
Mary Gore <dm101mary@gmail.com>
2023-02-14 01:30:00 +0400

    update path in map icon

--- src/plugins/yandex maps/map.js
+++ src/plugins/yandex maps/map.js
@@ -7,7 +7,7 @@ function init () {
 
     const placemark = new ymaps.Placemark(center, {}, {
       iconLayout: 'default#image',
-      iconImageHref: 'icons/iconLocationFill.svg',
+      iconImageHref: '../../icons/iconLocationFill.svg',
       iconImageSize: [32, 45],
       iconImageOffset: [0, -30]
     });
